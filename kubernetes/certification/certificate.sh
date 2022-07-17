048fbe86d8ab7c8b67731cafc1627a47aff4f87d
Ric <103566072+Riccardo-Bertelli@users.noreply.github.com>
2022-07-17 18:32:19 +0200

    Aggiunto certificato TLS e Jaeger

---
+++ kubernetes/certification/certificate.sh
@@ -0,0 +1,7 @@
+#Operazioni per creazione certificato:
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=CA Inc./CN=CA.com' -keyout CA.key -out CA.crt
+
+openssl req -out 34.65.90.250.nip.io.csr -newkey rsa:2048 -nodes -keyout 34.65.90.250.nip.io.key -subj "/CN=rc-project/O=rc-project"
+openssl x509 -req -sha256 -days 365 -CA CA.crt -CAkey CA.key -set_serial 0 -in 34.65.90.250.nip.io.csr -out 34.65.90.250.nip.io.crt
+
+kubectl create -n istio-system secret tls frescocredentials --key=34.65.90.250.nip.io.key --cert=34.65.90.250.nip.io.crt
--- kubernetes/initial_config.sh
+++ kubernetes/initial_config.sh
@@ -1,4 +1,5 @@
 sudo apt update
+
 #installazione docker
 sudo apt install docker.io
 
@@ -6,13 +7,31 @@ sudo apt install docker.io
 curl -sfL https://get.k3s.io | sh -s - --no-deploy=traefik --write-kubeconfig-mode 644
 
 #installazione istio
-#curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.1 TARGET_ARCH=x86_64 sh -
-#cd istio-1.14.1
-#export PATH=$PWD/bin:$PATH
-#export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-#istioctl install --set profile=demo -y
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.1 TARGET_ARCH=x86_64 sh -
+cd istio-1.14.1
+export PATH=$PWD/bin:$PATH
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+istioctl install --set profile=demo -y
 
-#sleep 10
+sleep 10
+
+kubectl label namespace default istio-injection=enabled
 
 #apply bookinfo
-#kubectl label namespace default istio-injection=enabled
+#kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
+sleep 15
+
+#verifica corretto apply
+#kubectl exec "$(kubectl get pod -l app=ratings -o jsonpath='{.items[0].metadata.name}')" -c ratings -- curl -sS productpage:9080/productpage | grep -o "<title>.*</title>"
+
+#open the application to outside traffic
+#kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+#istioctl analyze
+
+#aggiungiamo gli addons di tracing
+kubectl apply -f samples/addons
+kubectl rollout status deployment/kiali -n istio-system
+
+#simuliamo traffico
+#export GATEWAY_URL={EXTERNAL_IP}.nip.io
+#for i in $(seq 1 100); do curl -s -o /dev/null "http://$GATEWAY_URL/productpage"; done
