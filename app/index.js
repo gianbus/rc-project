d5449fb9f1737fcbcd251de7ca6ca38e8f323ac8
Gianpaolo Busillo <83918379+gianbus@users.noreply.github.com>
2022-07-18 10:00:27 +0200

    Initialize new branch

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const req = require('request');
+const request = require('request');
 const {Console} = require('console');
 const express = require('express');
 const cookieParser = require('cookie-parser');
@@ -13,7 +13,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 ////////////////////API KEYS////////////////////
-//require('dotenv').config({ path: 'api_keys.env' })
+require('dotenv').config({ path: 'api_keys.env' })
 const visualWeatherKey = process.env.VISUAL_WEATHER_KEY;
 // credentials - get the credentials from the environment variables
 const client_id = process.env.CLIENT_ID;
@@ -28,69 +28,10 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const MAX_ADD_DAYS = 6;
 
 ////////////////////FUNCTIONS////////////////////
-// getLatLong - get the latitude and longitude of the event location
-function getLatLong(location, callback){
-  if(location === undefined) return null;
-  let location_utf8 = location.toString("utf8"); //convert to utf8
-  
-  req.get(`http://nominatim.openstreetmap.org/search?format=json&q=${location_utf8}`, (err, res, body) => { //get the lat and lon of the location
-    if (err){
-      return console.log(err); //if there is an error
-    }else{
-      data = JSON.parse(body); //parse the json
-      if(data.length > 0){ //location is found
-        let lat = JSON.parse(body)[0].lat; //get the lat
-        let lon = JSON.parse(body)[0].lon; //get the lon
-        return callback([lat, lon]); //callback the lat and lon
-
-      }else{ //location is not found
-        let splittato = location_utf8.split(/,/g); //split the location
-        if(splittato.length > 3){ //if the location is splitted
-          splittato.shift(); //remove the first element
-          return getLatLong(splittato.toString("utf8"), callback); //get the lat and lon of the second part of the location
-
-        }else{ //if the location is not splitted
-          console.log("Location not found for the event with location: "+location); //print the location not found
-          return callback([null,null]); //callback null
-        }
-      }
-    }
-
-  });
-}
-
 // toTimestamp - convert a date to a timestamp
 function toTimestamp(strDate){ //convert date to timestamp
   var datum = Date.parse(strDate);
   return datum/1000;
-}
-
-// getWeather - get the weather of the event location by using the lat and lon
-function getWeather(lat, lon, time, callback){
-  if(lat == null|| lon == null) return callback(null); //if the lat or lon is undefined
-  else{
-    console.log("Getting weather for: "+lat+" "+lon+" "+time+"...");
-    isMidNight(time,(mezzanotte) => {
-      if(mezzanotte) time = time + 1;
-      //let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric&include=current`;
-      let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric`;
-      //console.log(visualWeatherUrl);
-      req.get(visualWeatherUrl, (err, res, body) => {
-        if (err) return console.log(err);
-        else{
-          data = JSON.parse(body);
-          //console.log(data);
-          callback(data);
-        }
-      });
-    });
-  }
-}
-
-// isMidNight - check if the time is midnight
-function isMidNight(timestamp, callback){
-  let date = new Date(timestamp*1000);
-  return callback(date.getHours() == 0 && date.getMinutes() == 0);
 }
 
 // addDays - add days to a date and return the new date
@@ -130,11 +71,13 @@ function linkWeatherToEvents(res, events){
 
         console.log(i + ` - ${start} - ${event.summary}`); //print the event Summary
 
-        getLatLong(events[i].location, ([lat, lon]) => {
-          
-          getWeather(lat, lon, toTimestamp(start), (data) => {
+        request.get(`http://localhost:3000?location=${events[i].location}`,  (err_coord, res_coord, body_coord)=> {
+          let lat = (JSON.parse(body_coord)).lat;
+          let lon = (JSON.parse(body_coord)).lon;
+
+          request.get(`http://localhost:3001?lat=${lat}&lon=${lon}&time=${toTimestamp(start)}`, (err_weather, res_weather, body_weather) => {
+          let data = JSON.parse(body_weather).weather;
             if(data != null){ //if the weather is found
-              let temp = ((data.currentConditions.temp)*10)/10; // get the temperature in celsius (round to 1 decimal)
               let hour = event.start.date?0:(new Date(start)).getHours()
 
               
@@ -145,7 +88,7 @@ function linkWeatherToEvents(res, events){
             }
           
             if(++counter ===events.length){ //if all the events have been processed
-              console.log(events);
+              //console.log(events);
               res.render('index', { events }); //send the events to the client
               console.log("All events processed"); //print all events processed
             }
@@ -175,7 +118,7 @@ function linkWeatherToEvents(res, events){
 }
 ////////////////////API FUNCTIONS////////////////////
 function getPublicEvents(lat, lon, date, callback,category = "") {
-  req.get(
+  request.get(
     {
       url:''+`https://api.predicthq.com/v1/events/?end.lte=${date}&within=40km@${lat},${lon}&start.gte=${date}${category}`+'',
       headers: {
@@ -197,15 +140,21 @@ function getPublicEvents(lat, lon, date, callback,category = "") {
 }
 
 function apiWeatherEvents(req,res,category =""){
+  console.log("Getting weather events...");
   if(req.query.location === undefined){
     res.json({});
   }else{
     var counter = 0;
-    getLatLong(req.query.location, ([lat, lon]) => {
+    console.log(req.query.location)
+    request.get(`http://localhost:3000?location=${req.query.location}`,  (err_coord, res_coord, body_coord)=> {
+      console.log(body_coord);
+        let lat = (JSON.parse(body_coord)).lat;
+        let lon = (JSON.parse(body_coord)).lon;
         getPublicEvents(lat, lon, req.query.date,  (body) => {
             if(body.results.length > 0){
               body.results.map((event,i) => {
-                  getWeather(lat, lon, toTimestamp(event.start), (weather) => {
+                request.get(`http://localhost:3001?lat=${lat}&lon=${lon}&time=${toTimestamp(event.start)}`, (err_weather, res_weather, body_weather) => {
+                  let weather = JSON.parse(body_weather).weather;
                     if(weather != null){
                       let hour = (new Date(event.start)).getHours();
                       body.results[i].weather = weather.days[0].hours[hour]; //add the weather data to the event
@@ -317,7 +266,8 @@ app.get('/image', function(req, res){ //index page
 });
 
 app.get('/weather', function(req, res){ //index page
-  getWeather(req.query.lat, req.query.lon, parseInt(req.query.time), (data) => {
+  request.get(`http://localhost:3001?lat=${req.query.lat}&lon=${req.query.lon}&time=${parseInt(req.query.time)}`, (err_weather, res_weather, body_weather) => {
+    let data = (JSON.parse(body_weather)).weather;
     res.json(data.days[0].hours[(new Date(parseInt(req.query.time*1000))).getHours()]);
   });
 });
