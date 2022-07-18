4cfde793e598e0101b6856994d0ab86791d0b8a3
Ric <103566072+Riccardo-Bertelli@users.noreply.github.com>
2022-07-18 12:39:03 +0200

    Cambio struttura in micro servizi

--- app/micro-svc-getWeather/index.js
+++ app/micro-svc-getWeather/index.js
@@ -5,69 +5,58 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 
-require('dotenv').config({ path: '../api_keys.env' })
+//require('dotenv').config({ path: '../api_keys.env' })
 const visualWeatherKey = process.env.VISUAL_WEATHER_KEY;
 
 // getWeather - get the weather of the event location by using the lat and lon
 function getWeather(lat, lon, time, callback){
-    if(lat == null|| lon == null) return callback(null); //if the lat or lon is undefined
-    else{
-      console.log("Getting weather for: "+lat+" "+lon+" "+time+"...");
-      isMidNight(time,(mezzanotte) => {
-        if(mezzanotte) time = time + 1;
-        //let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric&include=current`;
-        let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric`;
-        //console.log(visualWeatherUrl);
-        request.get(visualWeatherUrl, (err, res, body) => {
-          if (err) return console.log(err);
-          else{
-            data = JSON.parse(body);
-            //console.log(data);
-            callback(data);
-          }
-        });
+  if(lat == null|| lon == null) return callback(null); //if the lat or lon is undefined
+  else{
+    console.log("Getting weather for: "+lat+" "+lon+" "+time+"...");
+    isMidNight(time,(mezzanotte) => {
+      if(mezzanotte) time = time + 1;
+      //let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric&include=current`;
+      let visualWeatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${time}?key=${visualWeatherKey}&unitGroup=metric`;
+      //console.log(visualWeatherUrl);
+      request.get(visualWeatherUrl, (err, res, body) => {
+        if (err) return console.log(err);
+        else{
+          data = JSON.parse(body);
+          //console.log(data);
+          callback(data);
+        }
       });
-    }
+    });
   }
+}
 
 // isMidNight - check if the time is midnight
 function isMidNight(timestamp, callback){
-    let date = new Date(timestamp*1000);
-    return callback(date.getHours() == 0 && date.getMinutes() == 0);
-  }
+  let date = new Date(timestamp*1000);
+  return callback(date.getHours() == 0 && date.getMinutes() == 0);
+}
 
 app.get('/', function(req, res){
-    console.log(req.query)
-    if(req.query.lat === undefined || req.query.lon === undefined || req.query.time === undefined){ //if the lat or lon is undefined
-        res.json(
-            {
-                weather: null
-            }); //null the weather
+  let lat = req.query.lat; //get the lat
+  let lon = req.query.lon; //get the lon
+  let time = parseInt(req.query.time); //get the time
+  getWeather(lat, lon, time, (weather) => { //get the weather of the location
+    if(weather === undefined){ //if the weather is undefined
+      res.json(
+        {
+          weather: null
+        }); //null the weather
     }else{
-        let lat = req.query.lat; //get the lat
-        let lon = req.query.lon; //get the lon
-        let time = req.query.time; //get the time
-        getWeather(lat, lon, time, (weather) => { //get the weather of the location
-            
-            if(weather === undefined){ //if the weather is undefined
-              
-                res.json(
-                    {
-                        weather: null
-                    }); //null the weather
-            }else{
-              console.log(weather)
-                res.json(
-                    {
-                        weather: weather
-                    }); //callback the weather
-            }
-        }
-        );
+      console.log(weather)
+      res.json(
+        {
+          weather: weather
+        }); //callback the weather
     }
+  }
+  );
 });
 
-
-app.listen(3001, function(){
-    console.log("Server running on port 3001");
+app.listen(80, function(){
+  console.log("microservice-getWeather running on port 80");
 });
---
+++ app/svc-api/index.js
@@ -0,0 +1,126 @@
+////////////////////REQUIREMENTS////////////////////
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+const request = require('request');
+const {Console} = require('console');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const { container } = require('googleapis/build/src/apis/container');
+let app = express();
+app.use(cookieParser());
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+////////////////////API KEYS////////////////////
+//require('dotenv').config({ path: '../api_keys.env' })
+
+// credentials - get the credentials from the environment variables
+const predict_key = process.env.PREDICT_KEY;
+////////////////////CONSTANTS////////////////////
+
+// Max days to add to the date
+const MAX_ADD_DAYS = 6;
+
+////////////////////FUNCTIONS////////////////////
+// toTimestamp - convert a date to a timestamp
+function toTimestamp(strDate){ //convert date to timestamp
+  var datum = Date.parse(strDate);
+  return datum/1000;
+}
+
+
+
+
+////////////////////API FUNCTIONS////////////////////
+function getPublicEvents(lat, lon, date, callback,category = "") {
+  request.get(
+    {
+      url:''+`https://api.predicthq.com/v1/events/?end.lte=${date}&within=40km@${lat},${lon}&start.gte=${date}${category}`+'',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer '+ predict_key
+      }
+    }, (err, res, body) => { //get the lat and lon of the location
+    
+    if (err){
+      return console.log(err); //if there is an error
+
+    }else{
+      //console.log(JSON.parse(body)); //parse the json
+      callback (JSON.parse(body))
+
+  }
+
+})
+}
+
+function apiWeatherEvents(req,res,category =""){
+  console.log("Getting weather events...");
+  if(req.query.location === undefined){
+    res.json({});
+  }else{
+    var counter = 0;
+    console.log(req.query.location)
+    request.get(`http://latlonservice?location=${req.query.location}`,  (err_coord, res_coord, body_coord)=> {   
+      let lat = (JSON.parse(body_coord)).lat;
+      let lon = (JSON.parse(body_coord)).lon;
+      if(lat && lon){
+        getPublicEvents(lat, lon, req.query.date,  (body) => {
+          console.log(body);
+          if(body.results.length > 0){
+            body.results.map((event,i) => {
+              request.get(`http://weatherservice?lat=${lat}&lon=${lon}&time=${toTimestamp(event.start)}`, (err_weather, res_weather, body_weather) => {
+                let weather = JSON.parse(body_weather).weather;
+                  if(weather != null){
+                    let hour = (new Date(event.start)).getHours();
+                    body.results[i].weather = weather.days[0].hours[hour]; //add the weather data to the event
+                  }
+                  if(++counter === body.results.length){ //if all the events have been processed
+                    if(req.query.maxtemperature && req.query.mintemperature){ //if the max and min temperature are requested
+                      res.json((body.results).filter((n)=> n.weather.temp <= req.query.maxtemperature && n.weather.temp >= req.query.mintemperature)); 
+                    }else if(req.query.mintemperature){
+                      res.json((body.results).filter((n)=> n.weather.temp >= req.query.mintemperature)); 
+                    }else if(req.query.maxtemperature){   
+                      res.json((body.results).filter((n)=> n.weather.temp <= req.query.maxtemperature)); 
+                    }else{
+                      res.json(body.results);
+                    }
+                    console.log("All events processed"); //print all events processed
+                  }
+                  
+                });
+            });
+          }else{
+            res.json({});
+          }
+        },category);
+      }else{ //if there is no location
+        res.json({error: "No location found"});
+      }
+    });
+  }
+}
+
+
+////////////////////END FUNCTIONS////////////////////
+
+
+////////////////////API CALLS////////////////////
+app.get('/api/v1/weatherEvents', function(req, res) {
+  apiWeatherEvents(req,res);
+});
+app.get('/api/v1/weatherEvents/concerts', function(req, res) {
+  apiWeatherEvents(req,res,"&category=concerts");
+});
+app.get('/api/v1/weatherEvents/sports', function(req, res) {
+  apiWeatherEvents(req,res,"&category=sports");
+});
+app.get('/api/v1/weatherEvents/community', function(req, res) {
+  apiWeatherEvents(req,res,"&category=community");
+});
+app.listen(80, function(){
+    console.log("Service-api running on port 80");
+  });
+////////////////////END MAIN////////////////////
+
